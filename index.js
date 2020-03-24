d05678086ca75aa665e79beaca835ad9d13a7eb4
nguynhieu <tthzicc@gmail.com>
2020-03-24 12:31:34 +0700

    Add js handle for form header

---
+++ index.js
@@ -0,0 +1,6 @@
+const custom = document.getElementById("custom");
+const menuCustom = document.getElementById("menu-custom");
+
+custom.addEventListener('click', function() {
+    menuCustom.classList.toggle("visibility");
+});
